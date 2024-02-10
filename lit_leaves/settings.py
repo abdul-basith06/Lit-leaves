a2f582ef593047adfc0e43643cb07e54ce7e32d3
abdul-basith06 <basitha85@gmail.com>
2024-02-10 10:32:42 +0530

    Email commenting

--- lit_leaves/settings.py
+++ lit_leaves/settings.py
@@ -20,22 +20,22 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'litleaves23@gmail.com'
-EMAIL_HOST_PASSWORD = 'xqds jioj yaoz qapz'  
-DEFAULT_FROM_EMAIL ='litleaves23@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'  
+# EMAIL_PORT = 587  
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'litleaves23@gmail.com'
+# EMAIL_HOST_PASSWORD = 'xqds jioj yaoz qapz'  
+# DEFAULT_FROM_EMAIL ='litleaves23@gmail.com'
 
 # Email configuration
-# EMAIL_BACKEND = config('EMAIL_BACKEND')
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 # Quick-start development settings - unsuitable for production
